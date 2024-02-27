447826795b295e26215459fd21a969597ab4fdd5
NekkidBear <jmking2009@live.com>
2024-02-26 23:42:30 -0600

    logged answer for question 17.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -171,6 +171,7 @@ console.log(luckyResult);
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
 
 result = adventurous ? "Adventures are great!" : "How about we stay home?"
+console.log(result);
 
 
 
